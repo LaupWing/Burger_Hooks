863bd03d7e94b32501a3c759e9fabbafa4980b74
LaupWing <loc_strict@hotmail.com>
2020-02-20 19:17:00 +0100

    513 modal reactmemo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Aux from '../../../hoc/Auxiliry/Auxiliry'
 import Button from '../../UI/Button/Button'
 
-class orderSummary extends Component{
-    componentDidUpdate(){
-        console.log('OrderSummary Update')
-    }
-    render(){
-        const ingredientsSummary = Object.keys(this.props.ingredients)
-            .map(igKey=>{
-                return <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
-                    </li>
-            })
-        return (
-            <Aux>
-                <h3>Your Order</h3>
-                <p>A Delicious burger with the following ingredients:</p>
-                <ul>
-                    {ingredientsSummary}
-                </ul>
-                <p><strong> Total Price {this.props.price.toFixed(2)}</strong></p>
-                <p>Continue to checkout?</p>
-                <Button btnType={'Danger'} clicked={this.props.purchaseCancel}>CANCEL</Button>
-                <Button btnType={'Success'} clicked={this.props.purchaseContinue}>CONTINUE</Button>
-            </Aux>
-        )
-    }
+const OrderSummary = props=>{
+    const ingredientsSummary = Object.keys(props.ingredients)
+        .map(igKey=>{
+            return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+        })
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A Delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p><strong> Total Price {props.price.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button btnType={'Danger'} clicked={props.purchaseCancel}>CANCEL</Button>
+            <Button btnType={'Success'} clicked={props.purchaseContinue}>CONTINUE</Button>
+        </Aux>
+    )
 }
 
-export default orderSummary
+export default OrderSummary
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 import classes from './Modal.module.css'
 import Aux from '../../../hoc/Auxiliry/Auxiliry'
 import Backdrop from '../Backdrop/Backdrop'
 
-class Modal extends Component{
-    shouldComponentUpdate(nextProps, nextState){
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children
-    }
-    componentDidUpdate(){
-        console.log('Modal will update')
-    }
-    render(){
-        return(
-            <Aux>
-                <Backdrop clicked={this.props.modalClosed} show={this.props.show}/>
-                <div
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }} 
-                    className={classes.Modal}
-                >
-                    {this.props.children} 
-                    {/* Now we can determine which componets or jsx elements goes in the this modal */}
-                </div>
-            </Aux>
-        )
-    }
+const Modal = props=>{
+    // shouldComponentUpdate(nextProps, nextState){
+    //     return nextProps.show !== this.props.show || nextProps.children !== this.props.children
+    // }
+    return(
+        <Aux>
+            <Backdrop clicked={props.modalClosed} show={props.show}/>
+            <div
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }} 
+                className={classes.Modal}
+            >
+                {props.children} 
+                {/* Now we can determine which componets or jsx elements goes in the this modal */}
+            </div>
+        </Aux>
+    )
 }
 
-export default Modal
+export default React.memo(Modal, (prevProps, nextProps)=>
+    nextProps.show === prevProps.show && 
+    nextProps.children === prevProps.children
+)
